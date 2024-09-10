c7de357ca0aa4daa4622aacc6d9d69547f9d8442
rozam007 <rozam0040@gmail.com>
2024-09-10 15:49:03 +0500

    create basic http server

--- How Node Works/events.js
+++ How Node Works/events.js
@@ -1,4 +1,5 @@
 const EventEmitter = require('events')
+const http = require('http')
 
 
 const myEmitter = new EventEmitter();
@@ -10,4 +11,33 @@ myEmitter.on('rozam', () => {
 
 
 myEmitter.emit('rozam')
+
+
+
+
+
+
+
+
+
+const server = http.createServer();
+
+server.on('request', (req, res)=>{
+    res.end('request received')
+})
+
+
+server.on('request', (req, res)=>{
+    console.log('another request received')
+})
+
+
+server.on('close', (req, res)=>{
+    console.log('server closed');
+})
+
+
+server.listen(8000, '127.0.0.1', () => {
+    console.log('server is listening at port 8000');
+})
 
